9e2e13647a075c097996005e4d5e9bad144800d0
princeyadav7 <133991180+princeyadav7@users.noreply.github.com>
2024-03-13 23:41:34 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (41.86%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int sum = ((n * (n + 1)) / 2), sumLeft = 0;
+
+        for (int i = 0; i <= n; i++) {
+            sumLeft += i;
+            int sumRight=sum-sumLeft+i;
+            if (sumLeft==sumRight) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
