2362fda24a1455870df5c10b008c69d6f5528ff9
princeyadav7 <133991180+princeyadav7@users.noreply.github.com>
2024-03-14 22:45:31 +0530

    Time: 42 ms (48.40%), Space: 12.8 MB (45.95%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& v, int t) {
+        int n=v.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(v[i]+v[j]==t) return {i,j};
+}
+   }
+        return {};
+        
+    }
+};
