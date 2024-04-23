b313f4fabb448018d339ae9f521146c779f45342
Prince Yadav <133991180+princeyadav7@users.noreply.github.com>
2024-04-23 10:24:42 +0530

    Time: 45 ms (46.16%), Space: 12.6 MB (73.70%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,7 +4,8 @@ public:
         int n=v.size();
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                if(v[i]+v[j]==t) return {i,j};
+                if(v[i]+v[j]==t) 
+                return {i,j};
 }
    }
         return {};
